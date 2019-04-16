e0fb297b32b0c276451e36e91b338f6ed8e0c9c7
Tung <koala5858@gmail.com>
2019-04-16 17:35:49 +0800

    終極密碼公開作品第一次上傳

---
+++ Lucky number game/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace GuestNumberAdv2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Answer = new System.Windows.Forms.Button();
+            this.lbRange = new System.Windows.Forms.Label();
+            this.enterbtn = new System.Windows.Forms.Button();
+            this.lbRes = new System.Windows.Forms.Label();
+            this.tbNO = new System.Windows.Forms.TextBox();
+            this.Mytitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Answer
+            // 
+            this.Answer.Font = new System.Drawing.Font("PMingLiU", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Answer.Location = new System.Drawing.Point(232, 318);
+            this.Answer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Answer.Name = "Answer";
+            this.Answer.Size = new System.Drawing.Size(109, 43);
+            this.Answer.TabIndex = 11;
+            this.Answer.Text = "看答案";
+            this.Answer.UseVisualStyleBackColor = true;
+            this.Answer.Click += new System.EventHandler(this.Answer_Click);
+            // 
+            // lbRange
+            // 
+            this.lbRange.AutoSize = true;
+            this.lbRange.Font = new System.Drawing.Font("PMingLiU", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbRange.Location = new System.Drawing.Point(250, 194);
+            this.lbRange.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbRange.Name = "lbRange";
+            this.lbRange.Size = new System.Drawing.Size(77, 27);
+            this.lbRange.TabIndex = 10;
+            this.lbRange.Text = "Range";
+            this.lbRange.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // enterbtn
+            // 
+            this.enterbtn.Font = new System.Drawing.Font("PMingLiU", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.enterbtn.Location = new System.Drawing.Point(232, 249);
+            this.enterbtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.enterbtn.Name = "enterbtn";
+            this.enterbtn.Size = new System.Drawing.Size(109, 43);
+            this.enterbtn.TabIndex = 9;
+            this.enterbtn.Text = "確認";
+            this.enterbtn.UseVisualStyleBackColor = true;
+            this.enterbtn.Click += new System.EventHandler(this.enterbtn_Click);
+            // 
+            // lbRes
+            // 
+            this.lbRes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbRes.AutoSize = true;
+            this.lbRes.Font = new System.Drawing.Font("PMingLiU", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbRes.Location = new System.Drawing.Point(250, 145);
+            this.lbRes.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbRes.Name = "lbRes";
+            this.lbRes.Size = new System.Drawing.Size(77, 27);
+            this.lbRes.TabIndex = 8;
+            this.lbRes.Text = "Result";
+            this.lbRes.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tbNO
+            // 
+            this.tbNO.Font = new System.Drawing.Font("PMingLiU", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tbNO.Location = new System.Drawing.Point(265, 81);
+            this.tbNO.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbNO.Name = "tbNO";
+            this.tbNO.Size = new System.Drawing.Size(44, 39);
+            this.tbNO.TabIndex = 7;
+            // 
+            // Mytitle
+            // 
+            this.Mytitle.AutoSize = true;
+            this.Mytitle.BackColor = System.Drawing.SystemColors.Control;
+            this.Mytitle.Font = new System.Drawing.Font("PMingLiU", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Mytitle.ForeColor = System.Drawing.Color.Blue;
+            this.Mytitle.Location = new System.Drawing.Point(90, 18);
+            this.Mytitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Mytitle.Name = "Mytitle";
+            this.Mytitle.Size = new System.Drawing.Size(473, 38);
+            this.Mytitle.TabIndex = 6;
+            this.Mytitle.Text = "請輸入遊戲的最大數字範圍";
+            this.Mytitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 413);
+            this.Controls.Add(this.Answer);
+            this.Controls.Add(this.lbRange);
+            this.Controls.Add(this.enterbtn);
+            this.Controls.Add(this.lbRes);
+            this.Controls.Add(this.tbNO);
+            this.Controls.Add(this.Mytitle);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Answer;
+        private System.Windows.Forms.Label lbRange;
+        private System.Windows.Forms.Button enterbtn;
+        private System.Windows.Forms.Label lbRes;
+        private System.Windows.Forms.TextBox tbNO;     
+        private System.Windows.Forms.Label Mytitle;
+    }
+}
+
---
+++ Lucky number game/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuestNumberAdv2
+{
+    public partial class Form1 : Form
+    {
+        static int smallNO, bigNO ;
+        static int NO, minNO, maxNO;
+        bool again;
+        bool initialState;
+
+        //遊戲初始狀態設定
+        public void getInitialState()
+        {
+            Mytitle.Text = "請輸入遊戲的最大數字範圍";
+            smallNO = 1;            
+            minNO = 1;
+           
+            again = false;
+            initialState = true;
+            enterbtn.Text = "確認";
+            displayItem(!initialState);
+        }
+
+        //建立KeyPress事件
+        public Form1()
+        {
+            this.KeyPreview = true;
+            this.KeyPress += new KeyPressEventHandler(Form1_KeyPress);
+           
+            InitializeComponent();
+        }
+
+        //設定可以按enter輸入
+        void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                enter_method();              
+            }          
+        }
+
+        private void enterbtn_Click(object sender, EventArgs e)
+        {
+            enter_method();
+        }
+
+       
+        private void enter_method()
+        {
+            //遊戲開始
+            if (initialState && !again)
+            {
+                int maxNOValue;
+                             
+                if(!Int32.TryParse(tbNO.Text, out maxNOValue))
+                    maxNOValue = 100;
+
+                Mytitle.Text = "遊戲開始，請輸入一個數字";
+                enterbtn.Text = "確認";
+                initialState = false;
+                generateLucky(maxNOValue);
+                return;
+            }
+            //按下再一次
+            if (again)
+            {
+                getInitialState();
+                again = false;             
+                tbNO.Enabled = true;
+                tbNO.Text = "";
+            }
+            else
+            {
+                //使用者輸入數字時，判斷是否中獎
+                if (!Int32.TryParse(tbNO.Text, out int inputNO))
+                {
+                    if (tbNO.Text.Trim().ToLower() == "r")
+                        MessageBox.Show("答案是 " + NO.ToString());
+                    else
+                    {
+                        inputNO = -1;
+                        lbRes.Text = "錯誤!請重新輸入!";
+                        return;
+                    }
+                }
+
+                string resno;
+                Mathop m1 = new Mathop();
+                bool RangeChange = false;
+
+                if (inputNO != NO)
+                {
+
+                    if (inputNO < maxNO && inputNO > minNO)
+                        RangeChange = true;
+                    else
+                        return;
+                }
+                else if (inputNO == NO)
+                {
+                    finalResult();
+                    tbNO.Enabled = false;
+                    Answer.Enabled = false;
+                    enterbtn.Text = "再一次";
+                    again = true;
+                }
+                m1.a = NO;
+                m1.b = inputNO;
+                m1.evjudge();
+                resno = m1.result;
+                lbRes.Text = resno;
+                if (RangeChange)
+                    lbRange.Text = minNO + "~" + maxNO;
+            }
+        }
+      
+        //按下看答案
+        private void Answer_Click(object sender, EventArgs e)
+        {          
+            finalResult();
+        }
+
+        public void finalResult()
+        {
+            Mytitle.Text = "遊戲結束:Lucky Number 是 " + NO.ToString();
+            tbNO.Text = NO.ToString();
+            Answer.Enabled = false;
+            tbNO.Enabled = false;
+            enterbtn.Text = "再一次";
+            again = true;           
+        }
+ 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "終極密碼";
+            getInitialState();
+            displayItem(!initialState);          
+        }
+
+        //隱藏和顯示按鈕
+        public void displayItem(bool isShow)
+        {
+            lbRange.Visible = isShow;
+            lbRes.Visible = isShow;
+            Answer.Visible = isShow;
+        }
+
+        //產生LUCKY NUMBER，最大是maxValue
+        public void generateLucky(int maxValue)
+        {
+            bigNO = maxValue;
+            generateLucky();
+        }
+      
+        public void generateLucky()
+        {
+            displayItem(true);
+            Random rnum = new Random();
+            minNO = smallNO;
+            maxNO = bigNO;
+            NO = rnum.Next(minNO + 1, maxNO - 1);
+           // this.Text = NO.ToString();  //Display the code in tittle
+            lbRange.Text = smallNO + "~" + bigNO;
+            enterbtn.Text = "確認";
+            lbRes.Text = "開始";
+            Answer.Enabled = true;
+        }
+
+        //判斷數字太大或太小使用委派
+        public delegate void Mathopdlg();
+
+        class Mathop
+        {
+            public event Mathopdlg ev1, ev2, ev3;
+            public int a, b;
+            public string result;
+            public Mathop()
+            {
+                ev1 = new Mathopdlg(this.big);
+                ev2 = new Mathopdlg(this.small);
+                ev3 = new Mathopdlg(this.equal);
+            }
+            public void evjudge()
+            {
+                if (b > a)
+                    ev1();
+                else if (b < a)
+                    ev2();
+                else
+                    ev3();
+            }
+            public void big()
+            {
+                result = "太大了";
+                maxNO = b;
+            }
+            public void small()
+            {
+                result = "太小了";
+                minNO = b;
+            }
+            public void equal()
+            {
+                result = "命中!";
+            }
+        }
+    }
+}
